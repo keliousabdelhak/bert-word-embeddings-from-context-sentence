7440fc6b29372ff980c68aa6c509b9c3d98ae357
keliousabdelhak <keliousabdelhak@gmail.com>
2023-01-22 17:29:29 +0100

    update name

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
 VERSION = '0.0.1'
 
 DESCRIPTION = 'Extract bert word embedding from sentence (based on the context)'
@@ -11,12 +16,12 @@ LONG_DESCRIPTION = 'A package that allows to build word embedding from context u
 
 # Setting up
 setup(
-    name="word-embeddings-from-context",
+    name="word_embeddings_from_context",
     version=VERSION,
     author="Abdelhak KELIOUS",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
     packages=find_packages(),
     install_requires=['torch', 'pytorch-pretrained-bert'],
     keywords=['python', 'nlp', 'bert', 'text', 'transformers', 'embedding', 'word embedding', 'sentence embedding', 'context embedding', 'word embedding from context', 'sentence embedding from context', 'context embedding from context','similarity'],
